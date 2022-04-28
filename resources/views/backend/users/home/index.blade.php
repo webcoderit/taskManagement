f4755170812f0596e0413a9b64b4a2bb4d9eff3c
webcoderit <webcoderit@gmail.com>
2022-04-28 11:37:48 +0600

    profile fix

--- resources/views/backend/users/home/index.blade.php
+++ resources/views/backend/users/home/index.blade.php
@@ -10,9 +10,8 @@
             <div class="col">
                 <div class="card radius-10">
                     <div class="card-body text-center">
-                       <p class="mb-1">Sessions</p>
+                       <p class="mb-1">Admission</p>
                        <h3 class="mb-3">876</h3>
-                       <p class="font-13"><span class="text-success"><i class="lni lni-arrow-up"></i>2.1%</span> vs last 7 days</p>
                        <div id="chart1"></div>
                     </div>
                 </div>
@@ -20,9 +19,8 @@
             <div class="col">
                <div class="card radius-10">
                    <div class="card-body text-center">
-                      <p class="mb-1">Total Users</p>
+                      <p class="mb-1">Pending</p>
                       <h3 class="mb-3">4.5M</h3>
-                      <p class="font-13"><span class="text-success"><i class="lni lni-arrow-up"></i> 4.2% </span> last 7 days</p>
                       <div id="chart2"></div>
                    </div>
                </div>
@@ -30,9 +28,8 @@
            <div class="col">
                <div class="card radius-10">
                    <div class="card-body text-center">
-                      <p class="mb-1">Page Views</p>
+                      <p class="mb-1">Total Task</p>
                       <h3 class="mb-3">64,835</h3>
-                      <p class="font-13"><span class="text-danger"><i class="lni lni-arrow-down"></i> 3.6%</span> vs last 7 days</p>
                       <div id="chart3"></div>
                    </div>
                </div>
@@ -40,9 +37,8 @@
            <div class="col">
                <div class="card radius-10">
                    <div class="card-body text-center">
-                      <p class="mb-1">Bounce Rate</p>
+                      <p class="mb-1">Not Interested</p>
                       <h3 class="mb-3">42.68%</h3>
-                      <p class="font-13"><span class="text-success"><i class="lni lni-arrow-up"></i> 2.5%</span> vs last 7 days</p>
                       <div id="chart4"></div>
                    </div>
                </div>
@@ -50,9 +46,8 @@
            <div class="col col-md-12">
                <div class="card radius-10">
                    <div class="card-body text-center">
-                      <p class="mb-1">Avg. Session Duration</p>
+                      <p class="mb-1">Highly Interested</p>
                       <h3 class="mb-3">00:04:60</h3>
-                      <p class="font-13"><span class="text-danger"><i class="lni lni-arrow-down"></i> 5.2%</span> vs last 7 days</p>
                       <div id="chart5"></div>
                    </div>
                </div>
