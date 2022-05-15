272e4239085fec71ef000fb2de9310d2d648f79b
saidulislamjihad <saidulislamjihad20@gmail.com>
2022-05-15 18:17:33 +0600

    fix:Money receipt page design

--- resources/views/backend/users/task/money-receipt-pdf.blade.php
+++ resources/views/backend/users/task/money-receipt-pdf.blade.php
@@ -9,34 +9,33 @@
     <section>
         <table style="width: 700px;margin: auto;box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;border-collapse: collapse">
             <thead>
-                <tr style="display: flex;justify-content: space-between;width: 710px;">
-                    <td style="width: 85px;height: 75px;background: #f16522;border-radius: 0px 0px 100px 0px;">
-                    </td>
-                    <td style="display: inline-flex;align-items: center;justify-content: center;">
-                        <h4 style="background: #f16522;display: inline-block;padding: 5px 20px;font-size: 20px;font-weight: 500;color: #fff;border-radius: 0px 10px;margin-bottom: 0;">
+                <tr>
+                    <td colspan="2" style="text-align: center;">
+                       <h4 style="background: #f16522;display: inline-block;padding: 5px 20px;font-size: 20px;font-weight: 500;color: #fff;border-radius: 0px 10px;margin-bottom: 0;margin-top: 55px;margin-left: 60px;">
                             Money Receipt
-                        </h4>
+                        </h4> 
                     </td>
                     <td style="width:180px;">
-                        <img src="{{ asset('backend/') }}/assets/logo3.png" class="institute-logo">
-                        <p style="font-weight: 500;margin-bottom: 0;font-size: 13px;margin-top: 0;margin-bottom: 10px;">
+                        <img src="{{ asset('backend/') }}/assets/logo3.png" class="institute-logo" style="margin-left: 20px;">
+                        <p style="font-weight: 500;margin-bottom: 0;font-size: 13px;margin-top: 0;margin-bottom: 10px;margin-left: 20px;">
                             House#06, Level#03 Road-1/A, Sector#09 Housebuilding, Uttara Dhaka-1230
                         </p>
                     </td>
                 </tr>
             </thead>
             <tbody>
-                <tr style="border-bottom: 1px solid #ddd;display: flex;align-items: center; justify-content: space-between;width: 650px;padding: 0px 30px;">
-                    <td style="margin-bottom: 10px;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                <tr style="border-bottom: 1px solid #ddd;">
+                    <td style="padding: 0px 0px 10px 20px;">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Student ID :
                         </span>
                         <span style="color: #000;">
-                            {{ $data['moneyReceipt']->admissionForm->course }} - {{ $data['moneyReceipt']->admissionForm->student_id }}
+                           {{ $data['moneyReceipt']->admissionForm->course }} - {{ $data['moneyReceipt']->admissionForm->student_id }} 
                         </span>
                     </td>
-                    <td style="margin-bottom: 10px;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                    <td style="width: 180px;"></td>
+                    <td style="width: 200px;text-align: center;padding: 0px 20px 10px 0;">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Admission Date :
                         </span>
                         <span style="color: #000;">
@@ -44,9 +43,9 @@
                         </span>
                     </td>
                 </tr>
-                <tr style="border-bottom: 1px solid #ddd;display: flex;align-items: center; justify-content: space-between;width: 650px;padding: 0px 30px;">
-                    <td style="margin: 10px 0;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                <tr style="border-bottom: 1px solid #ddd;">
+                    <td colspan="3" style="padding: 10px 0 10px 20px;">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Student Name :
                         </span>
                         <span style="color: #000;">
@@ -54,41 +53,41 @@
                         </span>
                     </td>
                 </tr>
-                <tr style="border-bottom: 1px solid #ddd;display: flex;align-items: center; justify-content: space-between;width: 650px;padding: 0px 30px;">
-                    <td style="margin: 10px 0;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                <tr style="border-bottom: 1px solid #ddd;">
+                    <td colspan="3" style="padding: 10px 0 10px 20px;">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Course Name :
                         </span>
                         <span style="color: #000;">
                             @if($data['moneyReceipt']->admissionForm->course == 'web')
-                                    Full stack web development
-                                @elseif($data['moneyReceipt']->admissionForm->course == 'digital')
-                                    Advance Digital Marketing
-                                @else
-                                    Communication English
-                                @endif
+                                Full stack web development
+                            @elseif($data['moneyReceipt']->admissionForm->course == 'digital')
+                                Advance Digital Marketing
+                            @else
+                                Communication English
+                            @endif
                         </span>
                     </td>
                 </tr>
-                <tr style="border-bottom: 1px solid #ddd;display: flex;align-items: center; justify-content: space-between;width: 650px;padding: 0px 30px;">
-                    <td style="margin: 10px 0;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                <tr style="border-bottom: 1px solid #ddd;">
+                    <td style="padding: 10px 0 10px 20px;">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Total Fee :
                         </span>
                         <span style="color: #000;">
-                            {{ $data['moneyReceipt']->total_fee }} TK
+                             {{ $data['moneyReceipt']->total_fee }} TK
                         </span>
                     </td>
-                    <td style="margin: 10px 0;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                    <td style="text-align: center;">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Advance :
                         </span>
                         <span style="color: #000;">
                             {{ $data['moneyReceipt']->advance }} TK
                         </span>
                     </td>
-                    <td style="margin: 10px 0;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                    <td style="text-align: end;padding: 10px 20px 10px 0;">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Due :
                         </span>
                         <span style="color: #000;">
@@ -96,24 +95,24 @@
                         </span>
                     </td>
                 </tr>
-                <tr style="border-bottom: 1px solid #ddd;display: flex;align-items: center; justify-content: space-between;width: 650px;padding: 0px 30px;">
-                    <td style="margin: 10px 0;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                <tr style="border-bottom: 1px solid #ddd;">
+                    <td style="padding: 10px 0px 10px 20px;" colspan="2">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Received By :
                         </span>
                         <span style="color: #000;">
                             {{ auth()->user()->full_name }}
                         </span>
                     </td>
-                    <td style="margin: 10px 0;">
-                        <span style="font-size: 15px;color: #000;font-weight: 500;">
+                    <td style="text-align: center;padding: 10px 20px 10px 0;">
+                        <span style="font-size: 15px;color: #000;font-weight: 600;">
                             Authorised By :
                         </span>
                         <span>WebCoder-it</span>
                     </td>
                 </tr>
-                <tr style="text-align: center;">
-                    <td>
+                <tr>
+                    <td style="text-align: center;" colspan="3">
                         <a href="https://webcoder-it.com" target="_blank" style="color: #f16522;display: inline-block;font-size: 16px;margin: 15px 0;text-decoration: none;">
                             https://webcoder-it.com ,
                         </a>
@@ -121,7 +120,6 @@
                             01810139951-8
                         </a>
                     </td>
-                    <td></td>
                 </tr>
            </tbody>
         </table>
