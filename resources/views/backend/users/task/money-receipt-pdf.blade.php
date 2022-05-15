8bb991745df50139c6749f16abb0be3fa14a071f
webcoderit <webcoderit@gmail.com>
2022-05-15 18:24:39 +0600

    fix:money receipt refactor

--- resources/views/backend/users/task/money-receipt-pdf.blade.php
+++ resources/views/backend/users/task/money-receipt-pdf.blade.php
@@ -6,7 +6,6 @@
     <title>Money Receipt Pdf</title>
 </head>
 <body>
-    <section>
         <table style="width: 700px;margin: auto;box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;border-collapse: collapse">
             <thead>
                 <tr style="display: flex;justify-content: space-between;width: 710px;">
@@ -125,6 +124,5 @@
                 </tr>
            </tbody>
         </table>
-    </section>
 </body>
 </html>
