6dfb6e5b3ac87562ec147e30e94ad894fdec730c
saidulislamjihad <saidulislamjihad20@gmail.com>
2022-06-04 14:11:39 +0600

    fix:HRM dashboard

--- resources/views/backend/admin/hrm/index.blade.php
+++ resources/views/backend/admin/hrm/index.blade.php
@@ -76,7 +76,79 @@
             </div>
         </div>
     </div>
-   </div>
+    <div class="search-form-wrapper card p-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 m-auto">
+                    <form class="form-group">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <label for="employe_name" style="font-weight: 600; margin-bottom: 5px;">
+                                    Employe Name
+                                </label><br>
+                                <select name="employe_name" class="form-control">
+                                    <option selected disabled>--- Select Employee Name ---</option>
+                                    <option class="al amin saki">Al-Amin</option>
+                                    <option class="rakib">Rakib</option>
+                                    <option class="rony">Rony</option>
+                                    <option class="limon">Limon</option>
+                                    <option class="utso">Utso</option>
+                                    <option class="forid">Forid</option>
+                                    <option class="shibly">Shibly</option>
+                                    <option class="masum">Masum</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="date" style="font-weight: 600; margin-bottom: 5px;">
+                                    Date
+                                </label><br>
+                                <input type="date" name="date" class="form-control" placeholder="Date">
+                            </div>
+                            <div class="col-md-3">
+                                <div class="input-group" style="margin-top: 25px;">
+                                    <button type="submit" class="input-group-text btn btn-primary">Search</button>
+                                    <a href="#" class="input-group-text btn btn-danger">Clear</a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>        
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Employee Name</th>
+                            <th>Date</th>
+                            <th>batch</th>
+                            <th>Studenrt Name</th>                            
+                            <th>Student Phone</th>
+                            <th>Student Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>                    
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>                
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @push('script')
