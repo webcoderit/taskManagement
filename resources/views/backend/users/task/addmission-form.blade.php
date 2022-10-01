b0a76d570394f4c02046982bb52940773b593e9f
Saidul Islam <saidulislamjihad20@gmail.com>
2022-10-01 15:34:05 -0700

    fix:Admission form working

--- resources/views/backend/users/task/addmission-form.blade.php
+++ resources/views/backend/users/task/addmission-form.blade.php
@@ -536,78 +536,70 @@
         }
 
         $(document).ready(function(){
-        var current_fs, next_fs, previous_fs; //fieldsets
-        var opacity;
-        var current = 1;
-        var steps = $("fieldset").length;
+            var current_fs, next_fs, previous_fs; //fieldsets
+            var opacity;
+            var current = 1;
+            var steps = $("fieldset").length;
 
-        setProgressBar(current);
+            setProgressBar(current);
 
-        $(".next").click(function(){
+            $(".next").click(function(){
+                current_fs = $(this).parent();
+                next_fs = $(this).parent().next();
+                //Add Class Active
+                $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+                //show the next fieldset
+                next_fs.show();
+                //hide the current fieldset with style
+                current_fs.animate({opacity: 0}, {
+                step: function(now) {
+                    opacity = 1 - now;
+                    current_fs.css({
+                        'display': 'none',
+                        'position': 'relative'
+                    });
+                        next_fs.css({'opacity': opacity});
+                    },
+                        duration: 500
+                });
+                setProgressBar(++current);
+            });
 
-        current_fs = $(this).parent();
-        next_fs = $(this).parent().next();
+            $(".previous").click(function(){
 
-        //Add Class Active
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+            current_fs = $(this).parent();
+            previous_fs = $(this).parent().prev();
 
-        //show the next fieldset
-        next_fs.show();
-        //hide the current fieldset with style
-        current_fs.animate({opacity: 0}, {
-        step: function(now) {
-        // for making fielset appear animation
-        opacity = 1 - now;
+            //Remove class active
+            $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
 
-        current_fs.css({
-        'display': 'none',
-        'position': 'relative'
-        });
-        next_fs.css({'opacity': opacity});
-        },
-        duration: 500
-        });
-        setProgressBar(++current);
-        });
+            //show the previous fieldset
+            previous_fs.show();
 
-        $(".previous").click(function(){
-
-        current_fs = $(this).parent();
-        previous_fs = $(this).parent().prev();
-
-        //Remove class active
-        $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-        //show the previous fieldset
-        previous_fs.show();
-
-        //hide the current fieldset with style
-        current_fs.animate({opacity: 0}, {
-        step: function(now) {
-        // for making fielset appear animation
-        opacity = 1 - now;
-
-        current_fs.css({
-        'display': 'none',
-        'position': 'relative'
-        });
-        previous_fs.css({'opacity': opacity});
-        },
-        duration: 500
-        });
-        setProgressBar(--current);
-        });
-
-        function setProgressBar(curStep){
-        var percent = parseFloat(100 / steps) * curStep;
-        percent = percent.toFixed();
-        $(".progress-bar")
-        .css("width",percent+"%")
-        }
-
-        $(".submit").click(function(){
-        return false;
-        })
+            //hide the current fieldset with style
+            current_fs.animate({opacity: 0}, {
+            step: function(now) {
+            // for making fielset appear animation
+            opacity = 1 - now;
+            current_fs.css({
+                'display': 'none',
+                'position': 'relative'
+            });
+                previous_fs.css({'opacity': opacity});
+            },
+                duration: 500
+            });
+                setProgressBar(--current);
+            });
+            function setProgressBar(curStep){
+                var percent = parseFloat(100 / steps) * curStep;
+                percent = percent.toFixed();
+                $(".progress-bar")
+                .css("width",percent+"%")
+            }
+                $(".submit").click(function(){
+                return false;
+            })
 
         });
 
